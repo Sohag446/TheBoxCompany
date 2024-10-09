816aa912afeca6bd86012f7edc50f935fa37875b
Sohag466 <hossensohag466@gmail.com>
2024-10-09 23:35:56 +0600

    i hope my design is complete

--- script/javaScript.js
+++ script/javaScript.js
@@ -1,33 +1,17 @@
+$('.hero_body').slick({
+    margin:10,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    loop:true,
+    autoplay: true,
+});
 
-
-// const lefticon='<div class="project_demo_btn_back hover_effect"><img src="images/Vector_back.png" alt="Vector Back"><p>Back</p></div> ';
-
-// const righticon='<div class="project_demo_btn_next hover_effect"><p>Next</p><img src="images/Vector_next.png" alt="Vector Next"></div>';
-// $('.owl-carousel').owlCarousel({
-//     margin:0,
-//     items:1,
-//     loop:false,
-//     nav: true,
-//     navText:[
-//         lefticon,
-//         righticon,
-//     ],
-//     dots: true,
-//     autoWidth: false,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:1
-//         },
-//         1000:{
-//             items:1
-//         }
-//     }
-// })
-
-$('.project_slide').slick({
+$(document).ready(function() {
+    $('.project_demo.active .project_slide').slick({
     margin:10,
     rows: 2,
     dots: true,
@@ -39,16 +23,62 @@ $('.project_slide').slick({
     prevArrow:'<div class="project_demo_btn_back hover_effect"><img src="images/Vector_back.png" alt="Vector Back"><p>Back</p></div> ',
     nextArrow:'<div class="project_demo_btn_next hover_effect"><p>Next</p><img src="images/Vector_next.png" alt="Vector Next"></div>',
     loop:true,
+    });
+
+    $('.project_li').on('click', function() {
+        var selectedTab = $(this).data('tab');
+
+        $('.project_li').removeClass('active');
+
+        $(this).addClass('active');
+
+        $('.project_demo').removeClass('active');
+
+        if (selectedTab === 'all') {
+            $('#all').addClass('active');
+        } else if (selectedTab === 'commercial') {
+            $('#commercial').addClass('active');
+        } else if (selectedTab === 'residential') {
+            $('#residential').addClass('active');
+        } else if (selectedTab === 'other') {
+            $('#other').addClass('active');
+        }
+
+        $('.project_demo.active .project_slide').slick({
+            margin:10,
+            rows: 2,
+            dots: true,
+            arrows: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            prevArrow:'<div class="project_demo_btn_back hover_effect"><img src="images/Vector_back.png" alt="Vector Back"><p>Back</p></div> ',
+            nextArrow:'<div class="project_demo_btn_next hover_effect"><p>Next</p><img src="images/Vector_next.png" alt="Vector Next"></div>',
+            loop:true,
+                });
+    });
 });
 
-$('.hero_body').slick({
-    margin:10,
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    loop:true,
-    autoplay: true,
+
+$(document).ready(function() {
+    $('input').on('input', function() {
+        const $label = $(this).siblings('label');
+
+        if ($(this).val()) {
+            $label.addClass('disabled');
+        } else {
+            $label.removeClass('disabled');
+        }
+    });
+
+    $('select').on('change', function() {
+        const $label = $(this).siblings('label');
+
+        if ($(this).val()) {
+            $label.addClass('disabled');
+        } else {
+            $label.removeClass('disabled');
+        }
+    });
 });
